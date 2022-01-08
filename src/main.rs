549f71caa8c1959e7aa31712f12ee785fa22a492
Jan Varga <janvarga94@gmail.com>
2022-01-08 20:13:02 +0100

    yes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,4 @@
-#![windows_subsystem = "windows"] // dont use console
-                                  // comment line above when debugging
-
-use ::error_chain;
-
-error_chain::error_chain! {
-    foreign_links {
-        Io(std::io::Error);
-        HttpRequest(reqwest::Error);
-    }
-}
+// #![windows_subsystem = "windows"] // dont use console
 
 #[tokio::main]
 async fn main() {
@@ -19,51 +9,44 @@ async fn main() {
     .unwrap();
 
     loop {
-        get_print_data().await.unwrap();
+        get_print_data().await;
         tokio::time::sleep(tokio::time::Duration::from_secs(20)).await;
     }
 }
 
-async fn get_print_data() -> Result<()> {
+async fn get_print_data() {
     let client = reqwest::Client::new();
     let res = client
         .get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?start=1&limit=10&convert=USD")
-        .header("X-CMC_PRO_API_KEY", "")
+        .header("X-CMC_PRO_API_KEY", "45ea0bbe-b259-4a95-8e91-07b22c0d8772")
         .header("Accept", "application/json")
         .send()
-        .await?;
+        .await.unwrap();
 
-    let text_body = res.text().await?;
-    let json: serde_json::Result<serde_json::Value> = serde_json::from_str(&text_body);
+    let text_body = res.text().await.unwrap();
+    let json: serde_json::Value = serde_json::from_str(&text_body).unwrap();
 
-    match json {
-        Ok(json) => {
-            println!("=====RESULT=====");
+    println!("=====RESULT=====");
 
-            let coins = &json["data"].as_array().unwrap();
-            let allowed = ["Bitcoin", "Ethereum", "XRP"];
-            for one_coin in coins.iter() {
-                let name = &one_coin["name"].as_str().unwrap();
-                if allowed.contains(name) {
-                    let change = one_coin["quote"]["USD"]["percent_change_24h"]
-                        .as_f64()
-                        .unwrap();
-                    println!(
-                        "{} $ {:.2}, 24h {:.1}%",
-                        name,
-                        one_coin["quote"]["USD"]["price"].as_f64().unwrap(),
-                        change
-                    );
-                    if change.abs() >= 15.0 {
-                        sent_notification();
-                    }
-                }
+    let coins = &json["data"].as_array().unwrap();
+    let allowed = ["Bitcoin", "Ethereum", "XRP"];
+    for one_coin in coins.iter() {
+        let name = &one_coin["name"].as_str().unwrap();
+        if allowed.contains(name) {
+            let change = one_coin["quote"]["USD"]["percent_change_24h"]
+                .as_f64()
+                .unwrap();
+            println!(
+                "{} $ {:.2}, 24h {:.1}%",
+                name,
+                one_coin["quote"]["USD"]["price"].as_f64().unwrap(),
+                change
+            );
+            if change.abs() >= 15.0 {
+                sent_notification();
             }
         }
-        Err(_) => println!("not ok"),
     }
-
-    Ok(())
 }
 
 fn sent_notification() {
